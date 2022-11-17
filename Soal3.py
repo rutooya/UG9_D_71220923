e445c0468edc0aea2a16b563dbb3453760803470
Sharon <desharonrembet@gmail.com>
2022-11-17 16:33:18 +0700

    sudah

---
+++ Soal3.py
@@ -0,0 +1,8 @@
+inputan = input("Masukkan nama anda: ")
+pisah = inputan.split(" ")
+depan = pisah [0]
+belakang = pisah [1]
+matkul = input("Masukkan mata kuliah: ")
+grup = input("Masukkan grup: ")
+print("Haloo!", belakang, ",", depan)
+print(depan, "tergabung dalam kelas", matkul, "pada grup", grup)
